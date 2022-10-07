0b7535ef43aaa42fc37c71c9ed6a6f8499417ca6
Leonid <Chunl3e666@yandex.ru>
2022-10-07 17:22:26 +0300

    Fix: some bugs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
-import { useEffect } from 'REACT'
-import TG_API from './utils/api-constants'
-import Header from './components/Header/Header'
+import { useEffect } from 'react';
+import { TG_API } from './utils/api-constants';
+import Header from './components/Header/Header';
 function App() {
-
   useEffect(() => {
     TG_API.ready()
   }, []);
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
     </div>
   );
 }
-
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import { TG_API } from "../../utils/api-constants";
+import {TG_API} from "../../utils/api-constants";
 import Button from "../Button/Button";
 
 const header = () => {
